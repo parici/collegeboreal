15e77d6e0ddd2d8b471eb90a936d3f7939c366a6
Jean Olivier Gary Uppiah (Live@EDU) <300096005@prodeboaladmin3.borealc.on.ca>
2016-09-22 11:33:54 -0400

    ArragnementDuRouter

---
+++ MasterListDetail/app/AppRoute.js
@@ -0,0 +1,12 @@
+"use strict";
+var router_1 = require("nativescript-angular/router");
+var list_component_1 = require("./components/liste/list.component");
+var details_component_1 = require("./components/details/details.component");
+exports.routes = [
+    { path: "", component: list_component_1.ListComponent },
+    { path: "details", component: details_component_1.DetailComponent }
+];
+exports.APP_ROUTER_PROVIDERS = [
+    router_1.nsProvideRouter(exports.routes, {}),
+];
+//# sourceMappingURL=AppRoute.js.map
--- MasterListDetail/platforms/ios/MasterListDetail/app/main.ts
+++ MasterListDetail/platforms/ios/MasterListDetail/app/main.ts
@@ -1,16 +1,20 @@
 // this import should be first in order to load some required settings (like globals and reflect-metadata)
 import {nativeScriptBootstrap} from "nativescript-angular/application";
-import {nsProvideRouter} from "nativescript-angular/router";
+//import {nsProvideRouter} from "nativescript-angular/router";
+import {APP_ROUTER_PROVIDERS} from "./AppRoute"
+
 import {RouterConfig} from "@angular/router";
 import {AppComponent} from "./app.component";
  
 import {ListComponent} from "./components/liste/list.component";
+
+
 //import {CreateComponent} from "./components/create/create.component";
  
-export const AppRoutes: RouterConfig = [
+/*export const AppRoutes: RouterConfig = [
     { path: "", component: ListComponent }//,
     //il ne peut pas trouver la classe DetailComponent car je pense que la route n'est bonne 
     //{ path: "../details/", component: DetailComponent}
-]
+]*/
  
-nativeScriptBootstrap(AppComponent, [[nsProvideRouter(AppRoutes, {})]]);
+nativeScriptBootstrap(AppComponent, [APP_ROUTER_PROVIDERS ]);
